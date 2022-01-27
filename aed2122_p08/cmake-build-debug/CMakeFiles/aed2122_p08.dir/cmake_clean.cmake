7646a37bcbd310f9ab13f75e0a0bc79847945c59
brunabrasil <up202007191@g.uporto.pt>
2022-01-27 17:09:32 +0000

    a) b) e c) p8

---
+++ aed2122_p08/cmake-build-debug/CMakeFiles/aed2122_p08.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../Tests/aed2122_p08"
+  "../Tests/aed2122_p08.pdb"
+  "CMakeFiles/aed2122_p08.dir/Tests/funWithHashTables.cpp.o"
+  "CMakeFiles/aed2122_p08.dir/Tests/tests.cpp.o"
+  "CMakeFiles/aed2122_p08.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aed2122_p08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
